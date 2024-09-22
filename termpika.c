01c9197ffe40e689c8a90ab078bb0f878edb00cc
b0urn3 <154826956+q4n0@users.noreply.github.com>
2024-09-22 15:27:05 +0300

    Update termpika.c

--- termpika.c
+++ termpika.c
@@ -23,11 +23,33 @@ char* get_current_time() {
     return buffer;
 }
 
+// Function to get the distribution type
+char* get_distro() {
+    static char distro[256];
+    FILE *fp = fopen("/etc/os-release", "r");
+    if (fp) {
+        while (fgets(distro, sizeof(distro), fp)) {
+            if (strncmp(distro, "PRETTY_NAME=", 12) == 0) {
+                // Remove "PRETTY_NAME=" and the quotes
+                char *start = strchr(distro, '"') + 1;
+                char *end = strrchr(distro, '"');
+                if (end) {
+                    *end = '\0';
+                }
+                return start;
+            }
+        }
+        fclose(fp);
+    }
+    return "Unknown Distro";
+}
+
 int main() {
     struct utsname unameData;
     char *timeStr = get_current_time();
     char *user = getenv("USER");
     uname(&unameData);
+    char *distro = get_distro();
 
     // Banner with user, host, time, kernel version, and distro name information
     printf(RED
@@ -42,7 +64,7 @@ int main() {
     "⢸⡇⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢛⣵⠟⠀⠀⣼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
     "⠈⠻⣆⠀⠀⠀⠀⠀⣻⠾⢿⣿⣛⣛⣻⣯⣵⡾⠛⠁⠀⠀⢀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
     "⠀⠀⠙⠷⣄⣀⣀⣠⡿⠀⠀⠀⠀⠀⠀⠀⠀⢷⡀⠀⠀⢀⣾⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
-    "⠀⠀⠀⠀⠈⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⠾⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+    "⠀⠀⠀⠀⠈⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⠾⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
     RESET WHITE
     "                             USER: %s\n"
     "                             HOST: %s\n"
@@ -50,7 +72,7 @@ int main() {
     "                             KERNEL: %s\n"
     "                             DISTRO: %s\n"
     RESET,
-    user, unameData.nodename, timeStr, unameData.release, unameData.sysname);
+    user, unameData.nodename, timeStr, unameData.release, distro);
 
     return 0;
 }
