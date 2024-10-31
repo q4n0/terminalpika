eedeed66ff4a9c50ab6616bb732ca07bd524b4c7
b0urn3 <154826956+q4n0@users.noreply.github.com>
2024-10-31 21:31:36 +0300

    Update termpika.c

--- termpika.c
+++ termpika.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <time.h>
 #include <sys/utsname.h>
+#include <sys/sysinfo.h>
+#include <unistd.h>
 
 #define RESET   "\033[0m"
 #define RED     "\033[31m"
@@ -10,6 +12,7 @@
 #define GREY    "\033[90m"
 #define WHITE   "\033[37m"
 
+// [Previous utility functions remain the same]
 char* get_current_time() {
     time_t t;
     struct tm *tm_info;
@@ -20,6 +23,51 @@ char* get_current_time() {
     return buffer;
 }
 
+char* get_shell() {
+    char* shell = getenv("SHELL");
+    if (!shell) return "Unknown Shell";
+    return strrchr(shell, '/') ? strrchr(shell, '/') + 1 : shell;
+}
+
+char* get_cpu_info() {
+    static char cpu_info[256];
+    FILE *fp = fopen("/proc/cpuinfo", "r");
+    if (fp) {
+        char line[256];
+        char model_name[256] = "";
+        float mhz = 0.0;
+        while (fgets(line, sizeof(line), fp)) {
+            if (strncmp(line, "model name", 10) == 0) {
+                char* value = strchr(line, ':');
+                if (value) {
+                    strcpy(model_name, value + 2);
+                    model_name[strlen(model_name)-1] = '\0';
+                }
+            }
+            if (strncmp(line, "cpu MHz", 7) == 0) {
+                sscanf(line, "cpu MHz : %f", &mhz);
+            }
+        }
+        snprintf(cpu_info, sizeof(cpu_info), "%.1f GHz %s", mhz/1000, model_name);
+        fclose(fp);
+        return cpu_info;
+    }
+    return "Unknown CPU";
+}
+
+char* get_memory_info() {
+    static char mem_info[64];
+    struct sysinfo si;
+    if (sysinfo(&si) == 0) {
+        unsigned long total_ram = si.totalram * si.mem_unit / (1024 * 1024 * 1024);
+        unsigned long free_ram = si.freeram * si.mem_unit / (1024 * 1024 * 1024);
+        unsigned long used_ram = total_ram - free_ram;
+        snprintf(mem_info, sizeof(mem_info), "%luGB/%luGB", used_ram, total_ram);
+        return mem_info;
+    }
+    return "Unknown RAM";
+}
+
 char* get_distro() {
     static char distro[256];
     FILE *fp = fopen("/etc/os-release", "r");
@@ -28,9 +76,7 @@ char* get_distro() {
             if (strncmp(distro, "PRETTY_NAME=", 12) == 0) {
                 char *start = strchr(distro, '"') + 1;
                 char *end = strrchr(distro, '"');
-                if (end) {
-                    *end = '\0';
-                }
+                if (end) *end = '\0';
                 fclose(fp);
                 return start;
             }
@@ -40,32 +86,40 @@ char* get_distro() {
     return "Unknown Distro";
 }
 
-void print_banner_and_info(const char *user, const char *host, const char *timeStr, const char *kernel, const char *distro) {
+void print_banner_and_info(const char *user, const char *host, const char *timeStr, 
+                          const char *kernel, const char *distro, const char *shell,
+                          const char *cpu, const char *ram) {
     const char *ascii_art[] = {
-        "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡴⠶⠤⢤⣀",
-        "⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⡤⠶⠾⠷⠶⠶⣤⣈⣷",
-        "⠀⠀⠀⠀⠀⠀⣠⠾⢀⣤⣴⣶⣶⣶⣶⣦⣤⡉⠛⢦⡀",
-        "⠀⠀⠀⠀⢀⡾⢡⠞⠻⣿⣿⡿⢻⣿⣿⣿⣿⡿⠃⠀⠻⣆",
-        "⠀⠀⠀⠀⣾⢡⣿⠀⠀⣿⡟⠀⠀⣿⣿⣿⣿⣇⠀⠀⡀⢻⣆⣤⠶⠒⠒⠶⣤⡀",
-        "⠀⠀⠀⠀⣿⣾⣿⣄⣴⣿⣷⡀⣠⣿⣿⣿⣿⣿⣿⣿⣷⠘⣿⠧⣤⠻⠽⠂⢈⣿",
-        "⠀⠀⣠⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⢸⣿⡛⠛⣴⣲⠄⢸⡇",
-        "⢰⡟⠁⠀⠈⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢡⡿⠙⢿⣦⣤⣤⣤⠾⠃",
-        "⢸⡇⠀⠀⠀⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢛⣵⠟⠀⠀⣼⠃",
-        "⠈⠻⣆⠀⠀⠀⠀⠀⣻⠾⢿⣿⣛⣛⣻⣯⣵⡾⠛⠁⠀⠀⢀⣿",
-        "⠀⠀⠙⠷⣄⣀⣀⣠⡿⠀⠀⠀⠀⠀⠀⠀⠀⢷⡀⠀⠀⢀⣾⠃",
-        "⠀⠀⠀⠀⠈⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠳⠾⠛⠁"
+        "⠀⠀⠀⠀⢀⡠⠤⠔⢲⢶⡖⠒⠤⢄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⠀⠀⣠⡚⠁⢀⠀⠀⢄⢻⣿⠀⠀⠀⡙⣷⢤⡀⠀⠀⠀⠀⠀⠀",
+        "⠀⡜⢱⣇⠀⣧⢣⡀⠀⡀⢻⡇⠀⡄⢰⣿⣷⡌⣢⡀⠀⠀⠀⠀",
+        "⠸⡇⡎⡿⣆⠹⣷⡹⣄⠙⣽⣿⢸⣧⣼⣿⣿⣿⣶⣼⣆⠀⠀⠀",
+        "⣷⡇⣷⡇⢹⢳⡽⣿⡽⣷⡜⣿⣾⢸⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀",
+        "⣿⡇⡿⣿⠀⠣⠹⣾⣿⣮⠿⣞⣿⢸⣿⣛⢿⣿⡟⠯⠉⠙⠛⠓",
+        "⣿⣇⣷⠙⡇⠀⠁⠀⠉⣽⣷⣾⢿⢸⣿⠀⢸⣿⢿⠀⠀⠀⠀⠀",
+        "⡟⢿⣿⣷⣾⣆⠀⠀⠘⠘⠿⠛⢸⣼⣿⢖⣼⣿⠘⡆⠀⠀⠀⠀",
+        "⠃⢸⣿⣿⡘⠋⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⡆⠇⠀⠀⠀⠀",
+        "⠀⢸⡿⣿⣇⠀⠈⠀⠤⠀⠀⢀⣿⣿⣿⣿⣿⣿⣧⢸⠀⠀⠀⠀",
+        "⠀⠈⡇⣿⣿⣷⣤⣀⠀⣀⠔⠋⣿⣿⣿⣿⣿⡟⣿⡞⡄⠀⠀⠀",
+        "⠀⠀⢿⢸⣿⣿⣿⣿⣿⡇⠀⢠⣿⡏⢿⣿⣿⡇⢸⣇⠇⠀⠀⠀",
+        "⠀⠀⢸⡏⣿⣿⣿⠟⠋⣀⠠⣾⣿⠡⠀⢉⢟⠷⢼⣿⣿⠀⠀⠀",
+        "⠀⠀⠈⣷⡏⡱⠁⠀⠊⠀⠀⣿⣏⣀⡠⢣⠃⠀⠀⢹⣿⡄⠀⠀",
+        "⠀⠀⠘⢼⣿⠀⢠⣤⣀⠉⣹⡿⠀⠁⠀⡸⠀⠀⠀⠈⣿⡇⠀⠀"
     };
 
-    char info[5][50];
-    snprintf(info[0], 50, "USER: %s", user);
-    snprintf(info[1], 50, "HOST: %s", host);
-    snprintf(info[2], 50, "TIME: %s", timeStr);
-    snprintf(info[3], 50, "KERNEL: %s", kernel);
-    snprintf(info[4], 50, "DISTRO: %s", distro);
+    char info[8][100];
+    snprintf(info[0], 100, "USER: %s", user);
+    snprintf(info[1], 100, "HOST: %s", host);
+    snprintf(info[2], 100, "SHELL: %s", shell);
+    snprintf(info[3], 100, "DISTRO: %s", distro);
+    snprintf(info[4], 100, "KERNEL: %s", kernel);
+    snprintf(info[5], 100, "CPU: %s", cpu);
+    snprintf(info[6], 100, "RAM: %s", ram);
+    snprintf(info[7], 100, "TIME: %s", timeStr);
 
     int art_lines = sizeof(ascii_art) / sizeof(ascii_art[0]);
     int info_lines = sizeof(info) / sizeof(info[0]);
-    int start_info = 3;  // Start the info from the 4th line of ASCII art
+    int start_info = 3;  // Starting position for info text
 
     const char* colors[] = {RED, YELLOW, GREY};
     int color_count = sizeof(colors) / sizeof(colors[0]);
@@ -73,7 +127,7 @@ void print_banner_and_info(const char *user, const char *host, const char *timeS
     for (int i = 0; i < art_lines; i++) {
         printf("%s%s" RESET, colors[i % color_count], ascii_art[i]);
         if (i >= start_info && i < start_info + info_lines) {
-            printf(WHITE "%-40s" RESET, info[i - start_info]);
+            printf(WHITE "  %s" RESET, info[i - start_info]);
         }
         printf("\n");
     }
@@ -83,8 +137,13 @@ int main() {
     struct utsname unameData;
     char *timeStr = get_current_time();
     char *user = getenv("USER");
+    char *shell = get_shell();
+    char *cpu = get_cpu_info();
+    char *ram = get_memory_info();
     uname(&unameData);
     char *distro = get_distro();
-    print_banner_and_info(user, unameData.nodename, timeStr, unameData.release, distro);
+    
+    print_banner_and_info(user, unameData.nodename, timeStr, unameData.release, 
+                         distro, shell, cpu, ram);
     return 0;
 }
